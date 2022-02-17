260b9bd37c18b610f42b405ec3668993274ce8ce
yoon-junseo <ysj6844@naver.com>
2022-02-17 09:42:27 +0900

    feat: 리덕스 연결
    1. 리듀서를 합쳐서 반환
    2. index.js에서 devtool과 함께 합쳐서 스토어 생성
    #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import rootReducer from '@/modules';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
